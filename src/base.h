94a7f7f078b65f6ec6e3fd24d661edb982ada2c8
Anas Rchid <anasrchid@users.noreply.github.com>
2016-01-22 15:51:44 +0000

    Update base.h

--- src/base.h
+++ src/base.h
@@ -4,12 +4,21 @@
  #include <stdlib.h>
  #include <string.h>
  #include <time.h>
-
+  #define KNRM  "\x1B[0m"
+  #define KRED  "\x1B[31m"
+  #define KGRN  "\x1B[32m"
+  #define KYEL  "\x1B[33m"
+  #define KBLU  "\x1B[34m"
+  #define KMAG  "\x1B[35m"
+  #define KCYN  "\x1B[36m"
+  #define KWHT  "\x1B[37m"
+  #define RESET "\033[0m"
  
- #define ARRAY_SIZE 25
+ #define ARRAY_SIZE 1000
  #define RATE 9
  
  /*functions prototype*/
+
  int array_max(int*, int);//array_max() prototype
  int array_min(int*, int);//array_min() prototype
  void farray(int*, int);//farray() prototype
@@ -25,6 +34,10 @@
  int uniqarray(int*, int);
  long int sumarray(int*, int);
  int randinarray(int* , int );
+ void exparray(int* array, int b);
+ void imparray(int* array,int b);
  /*functions prototype*/
-
+  int arr[ARRAY_SIZE];
+  size_t a_size; 
+  char name[30];
 #endif
