5e03bbfb33ac57617bd8e14868b24a9106442ea2
Anas Rchid <anasrchid@users.noreply.github.com>
2016-01-22 15:52:30 +0000

    Update Menu.h

--- src/Menu.h
+++ src/Menu.h
@@ -1,7 +1,9 @@
 #ifndef __MENU_H_
  #define __MENU_H_
  #include "base.h"
+int chx;
  void menu(void);
  void arith_manip(void);
- void array(int* array, int b);
+ void ord_freq(void);
+ void other(void);
 #endif
